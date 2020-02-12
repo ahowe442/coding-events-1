969b7bfcbb14e043759a68679a97f1d337a0f44c
@ahowe442 <howe.abigail@gmail.com>
2020-02-11 19:57:27 -0600

    created a data layer

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,6 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
 import org.apache.coyote.Request;
+import org.launchcode.codingevents.data.eventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,12 +20,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        model.addAttribute("events", eventData.getAll());
         return "events/index";
     }
 
@@ -35,8 +34,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(new Event(eventName));
+    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
+        eventData.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -7,10 +7,19 @@ import java.util.Objects;
  */
 public class Event {
 
-    private String name;
+    private int id;
+    private static int nextId = 1;
 
-    public Event(String name) {
+
+
+    private String name;
+    private String description;
+
+    public Event(String name, String description) {
         this.name = name;
+        this.description = description;
+        this.id= nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -21,10 +30,34 @@ public class Event {
         this.name = name;
     }
 
-//I'm not sure why the @Override notation is above the toString method?
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //I'm not sure why the @Override notation is above the toString method?
     @Override
     public String toString() {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
