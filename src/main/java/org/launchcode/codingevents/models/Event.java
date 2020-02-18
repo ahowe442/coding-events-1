748e9f4703af373867cc0c559a14cd1e0e281506
@ahowe442 <howe.abigail@gmail.com>
2020-02-17 18:56:06 -0600

    Validating Models in the Controller/ handling Validation Errors using @Valid

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,6 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 /**
@@ -24,11 +22,29 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    @NotNull(message="This can not be empty")
+    @NotBlank(message="Please enter the address of the event location.")
+    private String location;
+
+    @AssertTrue(message = "Only accepts true values")
+    private boolean registration;
+
+    @Min(value = 1, message = "Number should be greater than zero attending")
+    private int attending;
+
+    @Size(min=8, max=12, message = "please enter a telephone number with digits only")
+    private String phoneNumber;
+
+    public Event(String name, String description, String contactEmail, String location,
+                 boolean registration, int attending, String phoneNumber) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.location=location;
+        this.registration=registration;
+        this.attending=attending;
+        this.phoneNumber=phoneNumber;
     }
 
     public Event() {
@@ -58,6 +74,38 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(boolean registration) {
+        this.registration = registration;
+    }
+
+    public int getAttending() {
+        return attending;
+    }
+
+    public void setAttending(int attending) {
+        this.attending = attending;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public int getId() {
