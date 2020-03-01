31c05a88c83daa09f9ef3b40def5424705dd4485
ahowe442 <howe.abigail@gmail.com>
2020-03-01 17:15:33 -0600

    Completed Exercise 17.4  need to enter user and password to connect the DB to Java

---
+++ src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
@@ -0,0 +1,7 @@
+package org.launchcode.codingevents.data;
+
+import org.launchcode.codingevents.models.Event;
+
+public interface EventCategoryRepository extends EventRepository<Event, Integer> {
+
+}
